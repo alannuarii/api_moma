1eef07c2a4fc74b16445164631b5cfb2426fa45c
Alan Nuari <89814176+alannuarii@users.noreply.github.com>
2025-08-19 11:41:40 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -461,8 +461,8 @@ def auto_schedule():
         object_rom.auto_upload_rom('rombss')
         object_weather.insert_weather()
         # post_max_irradiance()
-        post_mode_operasi()
-        mode_correction()
+        # post_mode_operasi()
+        # mode_correction()
     
         response = {"message": "Data berhasil dikirim"}
         return jsonify(response), 200
